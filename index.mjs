3cf54dc88cb113b2c6c244d2670de16b6a9a5b1d
Gary Jennings <garysjennings@gmail.com>
2023-09-02 14:26:43 -0600

    renamed contorllerInput to controller

--- index.mjs
+++ index.mjs
@@ -1,12 +1,12 @@
 import { makePixelCanvas } from './canvas/makePixelCanvas.mjs'
-import { setupKeyboard } from './controllerInput/index.mjs'
+import { setupKeyboard } from './controller/index.mjs'
 import { createEventBus } from './events/index.mjs'
 
 export const elementsCore = {
     canvas: {
         makePixelCanvas
     },
-    controllerInput: {
+    controller: {
         setupKeyboard
     },
     events: {
